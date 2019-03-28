f82898eec9efdd2d99347291ccdf5519bbedd82f
Diego Stewart <Diego@MacBook-Pro-de-Diego-2.local>
2019-03-28 20:28:02 +0100

    Correct Answer Logic and Animation

--- app/components/QuestionButtons.jsx
+++ app/components/QuestionButtons.jsx
@@ -7,24 +7,48 @@ export default class QuestionButtons extends React.Component {
     super(props);
   }
     
+  setListDisabled(list){
+    // list[0] = "disabled"; //amarillo
+    // list[1] = "disabled"; //verde
+    // list[2] = "disabled"; //azul
+    // list[3] = "disabled"; //marron
+    // list[4] = "disabled"; //puntolimpio
+    // list[5] = "disabled"; //sigre
+
+    list.amarillo = "nothing"; //amarillo
+    list.verde = "nothing"; //verde
+    list.azul = "nothing"; //azul
+    list.marron = "nothing"; //marron
+    list.puntolimpio = "nothing"; //puntolimpio
+    list.sigre = "nothing"; //sigre
+  }
+
   render(){
     let buttonQuestions ="";
     let nextQuestionButton="";
-    let disableButton=""; 
-
+    let disableButton={amarillo:"",verde:"",azul:"",marron:"",puntolimpio:"",sigre:""}; 
+    
     if(this.props.answered === true){        
       nextQuestionButton = <a onClick={this.props.onNextQuestion.bind(this)}><img src={"assets/images/Interfaz/flecha.png"} height="75" /></a>
-      disableButton="disabled";
+      this.setListDisabled(disableButton);
+      let bpress = this.props.buttonPressed;
+      if(this.props.correctAnswer){
+        disableButton[bpress] = "acierto";
+       }else{
+         let correct = this.props.question.solucion;
+        // disableButton[bpress] = "fallo"; // Esta se queda un poco mas visible que es la que has marcado
+          disableButton[correct] = "correcta";
+       }
       }
 
     
     let botones = [
-    <a onClick={this.props.onAnswer.bind(this, "amarillo")} key={"amarillo"} className={disableButton}><img src={"assets/images/Interfaz/cuboAmarillo.png"} height="100" /></a>,
-    <a onClick={this.props.onAnswer.bind(this, "verde")} key={"verde"} className={disableButton}><img src={"assets/images/Interfaz/cuboVerde.png"} height="100" /></a>,
-    <a onClick={this.props.onAnswer.bind(this, "azul")} key={"azul"} className={disableButton}><img src={"assets/images/Interfaz/cuboAzul.png"} height="100" /></a>,
-    <a onClick={this.props.onAnswer.bind(this, "marron")} key={"marron"} className={disableButton}><img src={"assets/images/Interfaz/cuboMarron.png"} height="100" /></a>,
-    <a onClick={this.props.onAnswer.bind(this, "puntolimpio")} key={"puntoLimpio"} className={disableButton}><img src={"assets/images/Interfaz/puntoLimpio.png"} height="100" /></a>,
-    <a onClick={this.props.onAnswer.bind(this, "sigre")} key={"sigre"} className={disableButton}><img src={"assets/images/Interfaz/Sigre.png"} height="100" /></a>,  
+    <a onClick={this.props.onAnswer.bind(this, "amarillo")} key={"amarillo"} className={disableButton.amarillo}><img className="fallo" src={"assets/images/Interfaz/cuboAmarillo.png"} height="100" /></a>,
+    <a onClick={this.props.onAnswer.bind(this, "verde")} key={"verde"} className={disableButton.verde}><img src={"assets/images/Interfaz/cuboVerde.png"} height="100" /></a>,
+    <a onClick={this.props.onAnswer.bind(this, "azul")} key={"azul"} className={disableButton.azul}><img src={"assets/images/Interfaz/cuboAzul.png"} height="100" /></a>,
+    <a onClick={this.props.onAnswer.bind(this, "marron")} key={"marron"} className={disableButton.marron}><img src={"assets/images/Interfaz/cuboMarron.png"} height="100" /></a>,
+    <a onClick={this.props.onAnswer.bind(this, "puntolimpio")} key={"puntoLimpio"} className={disableButton.puntolimpio}><img src={"assets/images/Interfaz/puntoLimpio.png"} height="100" /></a>,
+    <a onClick={this.props.onAnswer.bind(this, "sigre")} key={"sigre"} className={disableButton.sigre}><img src={"assets/images/Interfaz/Sigre.png"} height="100" /></a>,  
     ];
     
     let buttonRender = []; 
@@ -86,7 +110,8 @@ export default class QuestionButtons extends React.Component {
     }
 
       buttonQuestions = (
-        Utils.shuffleArray(buttonRender) 
+        //Utils.shuffleArray(buttonRender) 
+        buttonRender
       );    
 
       
