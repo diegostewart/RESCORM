c33325f8a126df3cf240a5c94766b5f338ba992b
Diego Stewart <Diego@MacBook-Pro-de-Diego-2.local>
2019-03-29 18:20:20 +0100

    Implementing Feedback Response

--- app/components/QuizQuestions.jsx
+++ app/components/QuizQuestions.jsx
@@ -2,32 +2,71 @@ import React from 'react';
 
 import * as Utils from '../vendors/Utils.js';
 import {objectiveAccomplished, objectiveAccomplishedThunk} from './../reducers/actions';
+import {Modal,Button} from 'react-bootstrap';
+
 
 import QuestionButtons from './QuestionButtons.jsx';
 
 export default class QuizQuestions extends React.Component {
   constructor(props){
     super(props);
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+
     this.state = {
       answered:false,
       correctAnswer:false,
-      buttonPressed:""
+      renderNext:false,
+      buttonPressed:"",
+      show: false,
+      feedback:""
     }
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
   }
 
   componentWillUpdate(prevProps, prevState){
     if(prevProps.question !== this.props.question){
       this.setState({answered:false});
       this.setState({correctAnswer:false});
+      this.setState({renderNext:false})
     }
   }
 
   onAnswer(answer){
     let acierto = 0;
-    if(this.props.question.solucion === answer){
+
+    const sleep = (milliseconds) => {
+      return new Promise(resolve => setTimeout(resolve, milliseconds))
+    }
+
+    if(this.props.question.solucion === answer){// Ha acertado la pregunta
       acierto +=1;
       this.setState({correctAnswer:true});
+      this.setState({renderNext:true})
+      if(this.props.question.feedback.positivo != "" && this.props.question.feedback.positivo != undefined){
+        console.log(this.props.question.feedback.positivo);
+        this.setState({feedback:this.props.question.feedback.positivo});
+        this.handleShow();
+
+      }
+    }else{    // Ha fallado la pregunta
+      sleep(2500).then(() => { 
+        this.setState({renderNext:true})
+        if(this.props.question.feedback.negativo != "" && this.props.question.feedback.positivo != undefined){
+        this.setState({feedback:this.props.question.feedback.negativo})
+        this.handleShow();
+        }
+      })
     }
+
     let objective = this.props.objective;
     this.props.dispatch(objectiveAccomplished(objective.id, objective.score * acierto));
     this.setState({answered:true});
@@ -35,12 +74,26 @@ export default class QuizQuestions extends React.Component {
     //this.props.onNextQuestion();
   }
 
+
   render(){
     return (
       <div className="question">
         <h1>{this.props.question.nombre}</h1>
         <img src={this.props.question.imagen}/>
-        <QuestionButtons I18n={this.props.I18n} question={this.props.question} correctAnswer={this.state.correctAnswer} buttonPressed={this.state.buttonPressed} answered={this.state.answered} difficulty={this.props.difficulty} onAnswer={this.onAnswer.bind(this)}  onNextQuestion={this.props.onNextQuestion.bind(this)} allow_finish={this.props.isLastQuestion}/>
+        <QuestionButtons I18n={this.props.I18n} question={this.props.question} correctAnswer={this.state.correctAnswer} renderNext={this.state.renderNext} buttonPressed={this.state.buttonPressed} answered={this.state.answered} difficulty={this.props.difficulty} onAnswer={this.onAnswer.bind(this)}  onNextQuestion={this.props.onNextQuestion.bind(this)} allow_finish={this.props.isLastQuestion}/>
+
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>FeedBack</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{this.state.feedback}</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        
       </div>
     );
   }
--- app/config/preguntas.js
+++ app/config/preguntas.js
@@ -8,7 +8,10 @@ export const lista_preguntas = {
         "solucion": "amarillo",
         "opciones": [], //Estos serían los botones que se renederizan.
         "dificultad": 1,
-        "imagen": "assets/images/Preguntas/Botella_Agua.jpg"
+        "imagen": "assets/images/Preguntas/Botella_Agua.jpg",
+        "feedback":{
+          "negativo":"Ohh esto es un feedback negativo"
+        }
         
       },
         {
@@ -16,64 +19,88 @@ export const lista_preguntas = {
         "solucion": "verde",
         "opciones": [],
         "dificultad": 1,
-        "imagen": "assets/images/Preguntas/Botella_Vino.jpeg"
-        
+        "imagen": "assets/images/Preguntas/Botella_Vino.jpeg",
+        "feedback":{
+          "positivo":"Esto es un feedback positivo",
+          "negativo":"Ohh esto es un feedback negativo"
+        }
       },
             {
         "nombre": "Papel de Periódico",
         "solucion": "azul",
         "opciones": [],
         "dificultad": 1,
-        "imagen": "assets/images/Preguntas/Periodico.jpeg"
-        
+        "imagen": "assets/images/Preguntas/Periodico.jpeg",
+        "feedback":{
+          "positivo":"Esto es un feedback positivo",
+          "negativo":"Ohh esto es un feedback negativo"
+        }
       },
       {
         "nombre": "Botella de Plastico-2",
         "solucion": "amarillo",
         "opciones": ["verde","azul"],
         "dificultad": 2,
-        "imagen": "assets/images/Preguntas/Botella_Agua.jpg"
-        
+        "imagen": "assets/images/Preguntas/Botella_Agua.jpg",
+        "feedback":{
+          "positivo":"Esto es un feedback positivo",
+          "negativo":"Ohh esto es un feedback negativo"
+        }
       },
         {
         "nombre": "Botella de Vino-2",
         "solucion": "verde",
         "opciones": ["sigre","azul"],
         "dificultad": 2,
-        "imagen": "assets/images/Preguntas/Botella_Vino.jpeg"
-        
+        "imagen": "assets/images/Preguntas/Botella_Vino.jpeg",
+        "feedback":{
+          "positivo":"Esto es un feedback positivo",
+          "negativo":"Ohh esto es un feedback negativo"
+        }
       },
             {
         "nombre": "Papel de Periódico-2",
         "solucion": "azul",
         "opciones": ["verde","puntolimpio"],
         "dificultad": 2,
-        "imagen": "assets/images/Preguntas/Periodico.jpeg"
-        
+        "imagen": "assets/images/Preguntas/Periodico.jpeg",
+        "feedback":{
+          "positivo":"Esto es un feedback positivo",
+          "negativo":"Ohh esto es un feedback negativo"
+        }
       },
       {
         "nombre": "Botella de Plastico-3",
         "solucion": "amarillo",
         "opciones": ["verde","azul"],
         "dificultad": 3,
-        "imagen": "assets/images/Preguntas/Botella_Agua.jpg"
-        
+        "imagen": "assets/images/Preguntas/Botella_Agua.jpg",
+        "feedback":{
+          "positivo":"Esto es un feedback positivo",
+          "negativo":"Ohh esto es un feedback negativo"
+        }
       },
         {
         "nombre": "Botella de Vino-3",
         "solucion": "verde",
         "opciones": ["marron","azul"],
         "dificultad": 3,
-        "imagen": "assets/images/Preguntas/Botella_Vino.jpeg"
-        
+        "imagen": "assets/images/Preguntas/Botella_Vino.jpeg",
+        "feedback":{
+          "positivo":"Esto es un feedback positivo",
+          "negativo":"Ohh esto es un feedback negativo"
+        }
       },
             {
         "nombre": "Papel de Periódico-3",
         "solucion": "azul",
         "opciones": ["verde","sigre"],
         "dificultad": 3,
-        "imagen": "assets/images/Preguntas/Periodico.jpeg"
-        
+        "imagen": "assets/images/Preguntas/Periodico.jpeg",
+        "feedback":{
+          "positivo":"Esto es un feedback positivo",
+          "negativo":"Ohh esto es un feedback negativo"
+        }
       }
     ]
 }
