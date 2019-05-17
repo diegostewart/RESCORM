ec5059f91bf002004f77955e3094a5fc3ddb999f
Diego Stewart <Diego@MacBook-Pro-de-Diego-2.local>
2019-05-17 18:34:48 +0200

    Importar Imagenes

--- app/config/preguntas.js
+++ app/config/preguntas.js
@@ -4,103 +4,365 @@ export const lista_preguntas = {
   "title":"Preguntas de Reciclaje",
   "questions":[
       {
-        "nombre": "Botella de Plastico",
+        "nombre": "Botella de Agua",
         "solucion": "amarillo",
         "opciones": ["verde", "amarillo"], //Estos serían los botones que se renederizan.
         "dificultad": 1,
-        "imagen": "assets/images/Preguntas/Botella_Agua.jpg",
+        "imagen": "assets/images/Preguntas/Amarillo/Botella_Agua.jpg",
         "feedback":{
           "negativo":"Ohh esto es un feedback negativo"
         }
         
       },
         {
-        "nombre": "Botella de Vino",
-        "solucion": "verde",
+        "nombre": "Brik de Leche",
+        "solucion": "amarillo",
         "opciones": ["verde","amarillo"],
         "dificultad": 1,
-        "imagen": "assets/images/Preguntas/Botella_Vino.jpeg",
+        "imagen": "assets/images/Preguntas/Amarillo/BrikLeche.png",
         "feedback":{
           "positivo":"Esto es un feedback positivo",
           "negativo":"Ohh esto es un feedback negativo"
         }
       },
             {
-        "nombre": "Papel de Periódico",
-        "solucion": "azul",
+        "nombre": "Carton de Zumo ",
+        "solucion": "amarillo",
         "opciones": ["verde", "amarillo","azul"],
         "dificultad": 1,
-        "imagen": "assets/images/Preguntas/Periodico.jpeg",
+        "imagen": "assets/images/Preguntas/Amarillo/CartonZumo.png",
         "feedback":{
           "positivo":"Esto es un feedback positivo",
           "negativo":"Ohh esto es un feedback negativo"
         }
       },
       {
-        "nombre": "Botella de Plastico-2",
+        "nombre": "Bote de Champú",
         "solucion": "amarillo",
         "opciones": ["verde","azul"],
-        "dificultad": 2,
-        "imagen": "assets/images/Preguntas/Botella_Agua.jpg",
+        "dificultad": 3,
+        "imagen": "assets/images/Preguntas/Amarillo/Champu.png",
         "feedback":{
           "positivo":"Esto es un feedback positivo",
           "negativo":"Ohh esto es un feedback negativo"
         }
       },
         {
-        "nombre": "Botella de Vino-2",
-        "solucion": "verde",
+        "nombre": "Comida en Conserva",
+        "solucion": "amarillo",
         "opciones": ["sigre","azul"],
         "dificultad": 2,
-        "imagen": "assets/images/Preguntas/Botella_Vino.jpeg",
+        "imagen": "assets/images/Preguntas/Amarillo/ConservaComida.png",
         "feedback":{
           "positivo":"Esto es un feedback positivo",
           "negativo":"Ohh esto es un feedback negativo"
         }
       },
             {
-        "nombre": "Papel de Periódico-2",
-        "solucion": "azul",
+        "nombre": "Desodorante",
+        "solucion": "amarillo",
         "opciones": ["verde","puntolimpio"],
-        "dificultad": 2,
-        "imagen": "assets/images/Preguntas/Periodico.jpeg",
+        "dificultad": 3,
+        "imagen": "assets/images/Preguntas/Amarillo/Desodorante.png",
         "feedback":{
           "positivo":"Esto es un feedback positivo",
           "negativo":"Ohh esto es un feedback negativo"
         }
       },
       {
-        "nombre": "Botella de Plastico-3",
+        "nombre": "Gel de Ducha",
         "solucion": "amarillo",
         "opciones": ["verde","azul"],
         "dificultad": 3,
-        "imagen": "assets/images/Preguntas/Botella_Agua.jpg",
+        "imagen": "assets/images/Preguntas/Amarillo/GelDucha.png",
         "feedback":{
           "positivo":"Esto es un feedback positivo",
           "negativo":"Ohh esto es un feedback negativo"
         }
       },
         {
-        "nombre": "Botella de Vino-3",
-        "solucion": "verde",
+        "nombre": "Refresco en lata",
+        "solucion": "amarillo",
         "opciones": ["marron","azul"],
-        "dificultad": 3,
-        "imagen": "assets/images/Preguntas/Botella_Vino.jpeg",
+        "dificultad": 2,
+        "imagen": "assets/images/Preguntas/Amarillo/LataRefresco.png",
         "feedback":{
           "positivo":"Esto es un feedback positivo",
           "negativo":"Ohh esto es un feedback negativo"
         }
       },
             {
-        "nombre": "Papel de Periódico-3",
+        "nombre": "Caja de Zapatos",
         "solucion": "azul",
+        "opciones": ["verde","sigre","azul"],
+        "dificultad": 1,
+        "imagen": "assets/images/Preguntas/Azul/CajaZapatos.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Hueveras de Cartón",
+        "solucion": "azul",
+        "opciones": ["verde","sigre","azul"],
+        "dificultad": 3,
+        "imagen": "assets/images/Preguntas/Azul/CartonHuevos.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Caja de Cereales",
+        "solucion": "azul",
+        "opciones": ["verde","sigre","azul"],
+        "dificultad": 2,
+        "imagen": "assets/images/Preguntas/Azul/Cereales.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Folio de Papel",
+        "solucion": "azul",
+        "opciones": ["verde","sigre","azul"],
+        "dificultad": 1,
+        "imagen": "assets/images/Preguntas/Azul/Folio.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Una Libreta",
+        "solucion": "azul",
+        "opciones": ["verde","sigre","azul"],
+        "dificultad": 3,
+        "imagen": "assets/images/Preguntas/Azul/Libreta.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Papel de Regalo",
+        "solucion": "azul",
+        "opciones": ["verde","sigre","azul"],
+        "dificultad": 2,
+        "imagen": "assets/images/Preguntas/Azul/PapelRegalo.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Un Periódico",
+        "solucion": "azul",
+        "opciones": ["verde","sigre","azul"],
+        "dificultad": 1,
+        "imagen": "assets/images/Preguntas/Azul/Periodico.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Una Revista",
+        "solucion": "azul",
+        "opciones": ["verde","sigre","azul"],
+        "dificultad": 1,
+        "imagen": "assets/images/Preguntas/Azul/Revista.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Restos de Berenjena",
+        "solucion": "marron",
+        "opciones": ["verde","sigre","marron"],
+        "dificultad": 1,
+        "imagen": "assets/images/Preguntas/Marron/Berenjena.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Bolsita de Te",
+        "solucion": "marron",
+        "opciones": ["verde","sigre","marron"],
+        "dificultad": 3,
+        "imagen": "assets/images/Preguntas/Marron/BolsitaTe.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Cascaras de Huevo",
+        "solucion": "marron",
+        "opciones": ["verde","sigre","marron"],
+        "dificultad": 2,
+        "imagen": "assets/images/Preguntas/Marron/CascaraHuevo.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Restos de Manzana",
+        "solucion": "marron",
+        "opciones": ["verde","sigre","marron"],
+        "dificultad": 1,
+        "imagen": "assets/images/Preguntas/Marron/Manzana.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Restos de Pescado",
+        "solucion": "marron",
+        "opciones": ["verde","sigre","marron"],
+        "dificultad": 2,
+        "imagen": "assets/images/Preguntas/Marron/Pescado.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Cascara de Plátano",
+        "solucion": "marron",
+        "opciones": ["verde","sigre","marron"],
+        "dificultad": 1,
+        "imagen": "assets/images/Preguntas/Marron/Platano.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Restos de Zanahoria",
+        "solucion": "marron",
+        "opciones": ["verde","sigre","marron"],
+        "dificultad": 1,
+        "imagen": "assets/images/Preguntas/Marron/Zanahoria.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Botella de Vino",
+        "solucion": "verde",
+        "opciones": ["verde","sigre"],
+        "dificultad": 1,
+        "imagen": "assets/images/Preguntas/Verde/Botella_vino.jpeg",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Botella de Cerveza",
+        "solucion": "verde",
+        "opciones": ["verde","sigre"],
+        "dificultad": 1,
+        "imagen": "assets/images/Preguntas/Verde/BotellaCerveza.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Botella de Refresco",
+        "solucion": "verde",
+        "opciones": ["verde","sigre"],
+        "dificultad": 1,
+        "imagen": "assets/images/Preguntas/Verde/BotellaRefresco.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Bote de Mermelada",
+        "solucion": "verde",
+        "opciones": ["verde","sigre"],
+        "dificultad": 2,
+        "imagen": "assets/images/Preguntas/Verde/Mermelada.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Un Perfume",
+        "solucion": "verde",
         "opciones": ["verde","sigre"],
         "dificultad": 3,
-        "imagen": "assets/images/Preguntas/Periodico.jpeg",
+        "imagen": "assets/images/Preguntas/Verde/Perfume.png",
         "feedback":{
-          "positivo":"Esto es un feedback positivo",
-          "negativo":"Ohh esto es un feedback negativo"
         }
-      }
+      }, {
+        "nombre": "Recipiente de Salsa",
+        "solucion": "verde",
+        "opciones": ["verde","sigre"],
+        "dificultad": 3,
+        "imagen": "assets/images/Preguntas/Verde/RecipienteSalsa.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Aceite Usado",
+        "solucion": "puntolimpio",
+        "opciones": ["verde","sigre","puntolimpio"],
+        "dificultad": 2,
+        "imagen": "assets/images/Preguntas/PuntoLimpio/AceiteOliva.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Una Bateria",
+        "solucion": "puntolimpio",
+        "opciones": ["verde","sigre","puntolimpio"],
+        "dificultad": 2,
+        "imagen": "assets/images/Preguntas/PuntoLimpio/Bateria.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Una Bombilla",
+        "solucion": "puntolimpio",
+        "opciones": ["verde","sigre","puntolimpio"],
+        "dificultad": 3,
+        "imagen": "assets/images/Preguntas/PuntoLimpio/Bombilla.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Copa de  Vino",
+        "solucion": "puntolimpio",
+        "opciones": ["verde","sigre","puntolimpio"],
+        "dificultad": 3,
+        "imagen": "assets/images/Preguntas/PuntoLimpio/CopaVino.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Un Frigorifico Antiguo",
+        "solucion": "puntolimpio",
+        "opciones": ["verde","sigre","puntolimpio"],
+        "dificultad": 1,
+        "imagen": "assets/images/Preguntas/PuntoLimpio/Frigorifico.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Una Jeringuilla Usada",
+        "solucion": "puntolimpio",
+        "opciones": ["marron","sigre","puntolimpio"],
+        "dificultad": 3,
+        "imagen": "assets/images/Preguntas/PuntoLimpio/Jeringuilla.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Un Teléfono Movil",
+        "solucion": "puntolimpio",
+        "opciones": ["verde","sigre","puntolimpio"],
+        "dificultad": 2,
+        "imagen": "assets/images/Preguntas/PuntoLimpio/Movil.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Un Neumatico",
+        "solucion": "puntolimpio",
+        "opciones": ["verde","sigre","puntolimpio"],
+        "dificultad": 2,
+        "imagen": "assets/images/Preguntas/PuntoLimpio/Neumatico.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Una Radiografia",
+        "solucion": "puntolimpio",
+        "opciones": ["verde","sigre","puntolimpio"],
+        "dificultad": 3,
+        "imagen": "assets/images/Preguntas/PuntoLimpio/Radiografia.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Un Termometro",
+        "solucion": "puntolimpio",
+        "opciones": ["verde","sigre","puntolimpio"],
+        "dificultad": 3,
+        "imagen": "assets/images/Preguntas/PuntoLimpio/Termometro.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Medicamentos Caducados",
+        "solucion": "sigre",
+        "opciones": ["verde","sigre","amarillo"],
+        "dificultad": 2,
+        "imagen": "assets/images/Preguntas/Sigre/MedicamentoCaducados.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Envases de Medicamentos ",
+        "solucion": "sigre",
+        "opciones": ["verde","sigre","amarillo"],
+        "dificultad": 2,
+        "imagen": "assets/images/Preguntas/Sigre/MedicamentoEnvase.png",
+        "feedback":{
+        }
+      }, {
+        "nombre": "Medicamentos que no Necesites",
+        "solucion": "sigre",
+        "opciones": ["verde","sigre","amarillo"],
+        "dificultad": 2,
+        "imagen": "assets/images/Preguntas/Sigre/MedicamentoNoNecesites.png",
+        "feedback":{
+        }
+      },
     ]
 }
