3b2272b857e5749b94b6920e390b8c9772ecac98
Diego Stewart <Diego@MacBook-Pro-de-Diego-2.local>
2019-05-19 14:07:21 +0200

    Info Screen Update

--- app/config/info_cubos.js
+++ app/config/info_cubos.js
@@ -4,7 +4,7 @@ export const info = {
       "depositar_Azul":[
         {
           "texto":"Este contenedor suele ser fácil de distinguir y de usar, ya que está destinado para papel y cartón exclusivamente. Un pequeño truco, es doblar y comprimir los envases en vez de arrugarlos, para ahorrar espacio en el cubo y aumentar su capacidad. Lo que debe meterse en el contenedor azul son envases de alimentación, calzado, productos congelados, papel para envolver, papel de uso diario… "
-        }, 
+        }
       ],
 
       "error_Azul":[
@@ -21,16 +21,16 @@ export const info = {
         {
           "texto":"Estos envases son trasladados a plantas de recuperación donde se limpian (plásticos, metales…), se clasifican según los tipos de papel y se prensan en balas de dimensiones y peso estandarizados. Una vez listos, se transportan hasta una fábrica de papel, donde se transforman en bobinas de papel nuevo, que se convertirán en cajas, de zapatos, cereales, folios, periódicos…"
         }, {
-          "texto":"1. En primer lugar, se pastifica el papel, es decir, se le añaden disolventes para que las fibras se separen."
+          "texto":"En primer lugar, se pastifica el papel, es decir, se le añaden disolventes para que las fibras se separen."
         }, {
-          "texto":"2. Después, se separa de todas las impurezas."
+          "texto":"Después, se separa de todas las impurezas."
         }, {
-          "texto":"3. A continuación, se centrifuga la mezcla para que los materiales se separen según su densidad."
+          "texto":"A continuación, se centrifuga la mezcla para que los materiales se separen según su densidad."
         }, {
-          "texto":"4. Se lava y se elimina la tinta con burbujas de aire."
+          "texto":"Se lava y se elimina la tinta con burbujas de aire."
         }, {
-          "texto":"5. Por último, se blanquea con peróxido de hidrógeno o hidrosulfito de sodio."
-        },
+          "texto":"Por último, se blanquea con peróxido de hidrógeno o hidrosulfito de sodio."
+        }
       ],
 
       "curiosidad_Azul":[
@@ -49,14 +49,238 @@ export const info = {
     
       "depositar_Amarillo":[
         {
-          "texto":"Texto primero"
+          "texto":"Dentro del contenedor amarillo, debemos depositar: botellas y envases de plástico, envases metálicos (latas, bandejas de aluminio, aerosoles, botes de desodorante…) y briks de leche, zumos o sopas."
+        }
+      ],
+
+      "error_Amarillo":[
+        {
+          "texto":"Es lógico pensar que todos los objetos que estén fabricados con plástico y que ya no nos son de utilidad, van al contenedor amarillo, pero no siempre es así."
         }, {
-          "texto":"Texto segundo"
+          "texto":"No deben depositarse: juguetes de plástico (que deben ir al contenedor de restos, aunque pueden donarse a muchas organizaciones que se dedican a obras sociales), biberones y chupetes (que también van al contenedor de restos), utensilios de cocina y los cubos de plástico (estos últimos también deben ir al contenedor de restos)."
         }, {
-          "texto":"Texto tercero"
+          "texto":"Otros materiales con los que también podemos dudar,  y que por tanto son impropios en el amarillo, son los siguientes: vasos de papel de cafeterías, papel plastificado usado en carnicerías y pescaderías, tupperwares, capsulas de aluminio de café, moldes de silicona para cocinar, termos, macetas de plástico, cajas de CD’s y DVD’s, carcasas de vídeos de VHS y cintas de casete, monederos y carteras, bolígrafos, sacapuntas, mecheros, materiales de plástico de cuadros o fotografías, envases de los medicamentos o neceseres"
+        }
+      ],
+
+      "reciclar_Amarillo":[
+        {
+          "texto":"Todos los envases que van a parar al contenedor amarillo son transportados a las plantas de selección, y de allí a sus respectivos recicladores."
         }, {
-          "texto":"texto cuarto"
-        },
+          "texto":"Primero se separan los materiales según sean aptos o no aptos. Por ejemplo, las etiquetas, los restos o tierras de algún tipo se desechan. Este proceso puede ser algo complejo, ya que se separan al menos tres fracciones: metales (acero y aluminio) y plásticos (PET, PEAD, film y plástico mezcla). Una botella de agua sería plástico PET, una lata de refresco sería metal y una bolsa de patatas sería plástico mezcla."
+        }, {
+          "texto":"Por otro lado, también se segregan según los colores. De esta forma se optimiza el uso de colorantes."
+        }, {
+          "texto":"Las piezas se rompen y trituran en trocitos pequeños para facilitar el tratamiento."
+        }, {
+          "texto":"Después se lavan, cubriendo los trozos con agua y dejando que las impurezas más densas queden abajo."
+        }, {
+          "texto":"Se seca y centrifuga, eliminando cualquier otra impureza que haya podido quedar."
+        }, {
+          "texto":"Se homogeneiza con un proceso mecánico para lograr un color y textura uniforme."
+        }, {
+          "texto":"Después vuelve a depurarse el material de impurezas y se le da la forma y el color demandado, pasando por los respectivos controles de calidad."
+        }
+      ],
+
+      "curiosidad_Amarillo":[
+        {
+          "texto":"¿Sabías que con 6 briks puede hacerse una caja de zapatos, 40 botellas de plástico pueden convertirse en un forro polar o que 80 latas de refresco pueden transformarse en una llanta de bicicleta?"
+        }, {
+          "texto":"8 botes de conserva pueden tener una segunda vida como olla de cocina, 22 botellas de plástico una camiseta y 550 latas pueden ser una silla."
+        }, {
+          "texto":"El contenedor amarillo es uno de los que más dudas genera. El contenedor amarillo lleva 20 años en funcionamiento en España, parece poco, ¿verdad? De media puede encontrarse un contenedor amarillo por cada 117 habitantes."
+        }, {
+          "texto":"A pesar de que ha habido una muy buena evolución, y cada año aumenta la cantidad de envases de plástico, briks y latas recicladas por habitante, un tercio de la población sigue sin saber con exactitud dónde va cada tipo de residuo."
+        } 
+      ],
+
+      "depositar_Verde":[
+        {
+          "texto":"Aquí debemos depositar botellas de vidrio (vino, cava…), frascos de vidrio (como perfumes o colonias) o tarros de alimentos (mermeladas, conservas, etc.)"
+        }
+      ],
+
+      "error_Verde":[
+        {
+          "texto":"Pero al igual que en el contenedor amarillo, también pueden encontrarse residuos impropios. ¡Ojo, es muy importante diferenciar el vidrio del cristal! Al verde solo debe ir al vidrio."
+        }, {
+          "texto":"Algunos errores comunes se dan cuando llega la hora de reciclar bombillas (quede deben llevares al punto limpio o a centros de recogida), y los espejos, ceniceros, vasos y vajillas también deben ser depositados en los mismos lugares."
+        }
+      ],
+
+      "reciclar_Verde":[
+        {
+          "texto":"El proceso comienza con la recogida. Los camiones que recogen el vidrio cuentan con una grúa incorporada que permite vaciar los contenedores rápidamente. Cuando están llenos, se llevan a las plantas de tratamiento, donde se lleva a cabo el siguiente proceso:"
+        }, {
+          "texto":"Se separa el material de las impurezas que puedan tener, como restos de líquidos, tapones, corchos…"
+        }, {
+          "texto":"Son llevadas por unas cintas transportadoras que incorporan un separador magnético para recoger todo el material metálico (tapones, tarros…)"
+        }, {
+          "texto":"Posteriormente se clasifica el vidrio según el tipo, mientras se sigue con la separación de impurezas, con chorros de agua que se activan mediante la detección de elementos opacos a través del vidrio."
+        }, {
+          "texto":"Por último, el vidrio es triturado, convirtiéndose en calcín. Este material es vidrio seleccionado, limpio y triturado. Además, necesita menor temperatura de fusión, pero fabrica materiales exactamente iguales que los primeros. ¡Así se gasta menos energía!"
+        }
+      ],
+
+      "curiosidad_Verde":[
+        {
+          "texto":"El vidrio es uno de esos materiales que puede reciclarse infinitamente sin perder ninguna de sus propiedades."
+        }, {
+          "texto":"De nuevo se recoge, se lleva a la planta de tratamiento para triturarlo y librarlo de impurezas para después ir a la fábrica de envases, donde se le dará una nueva vida."
+        }, {
+          "texto":"Habitualmente se transformará en un envase nuevo, aunque también pueden hacerse objetos de menaje, como por ejemplo un jarrón, entre otras muchas cosas."
+        }, {
+          "texto":"n el caso del contenedor verde, puede encontrarse uno cada 213 habitantes aquí en España. Se comenzó a reciclar el vidrio hace ya treinta años y desde entonces hemos avanzado mucho. El primer contenedor se puso en Barcelona en 1982. ¡Fue el primer contenedor de reciclaje en llegar a nuestro país!"
+        }, {
+          "texto":"El tipo de contenedor más habitual para este tipo de materiales suele ser el iglú, los que tienen forma redondeada y se parecen a las casitas de los esquimales, aunque también es habitual encontrarse otros tipos de contenedores."
+        }, {
+          "texto":"Desde 1998, la dotación de contenedores, gestionada por Ecovidrio, ha crecido más de un 200%.¡Impresionante! Actualmente, somos uno de los países con mejor ratio de contenedores (contenedores por número de habitantes). El 59% de hogares afirma que tienen uno a menos de 50 metros de su casa."
+        }
+      ],
+
+      "depositar_Marron":[
+        {
+          "texto":"Lo primero que debes saber es que no todas las Comunidades Autónomas cuentan con el contenedor marrón. Algunas de las pioneras son Euskadi, Navarra, Cataluña o Madrid y surge como división del contenedor gris."
+        }, {
+          "texto":"Lo que debe depositarse en este contenedor es: restos de alimentos como pieles de frutas, espinas de pescado, plantas, cascaras de huevo o posos; o servilletas y papel de cocina usados."
+        }
+      ],
+
+      "error_Marron":[
+        {
+          "texto":"Los restos impropios a este contenedor son los restos no orgánicos como objetos de cerámica, pañales, colillas, chicles, toallitas húmedas, arena para mascotas, pelo, polvo, etc."
+        }
+      ],
+
+      "reciclar_Marron":[
+        {
+          "texto":"Una vez se recogen los residuos, es preciso llevarlos a una compostadora que puede ser individual o comunitaria. De hecho, ¡puedes hacer compost en tu propia casa! "
+        }, {
+          "texto":"Se sigue un proceso sencillo que pretende imitar el de la naturaleza. Es exactamente igual al proceso habitual, pero controlando los parámetros de temperatura, humedad y oxígeno. De seis a doce meses después está listo para ser usado."
+        }, {
+          "texto":"Al separarlo del resto, se reduce la cantidad de residuos que llegan al vertedero, se reducen los malos olores y se ahorra energía, ya que puede producirse de esta manera en forma de biogás."
+        }, {
+          "texto":"Es importante saber dónde vamos a acumular los restos. Elegir un recipiente con posibilidad de cerrado es una buena opción. También es importante dónde va a estar el recipiente. Es preferible que sea un lugar cálido."
+        }, {
+          "texto":"Primero debe llenarse la capa de abajo del recipiente con una capa seca y leñosa, junto con algo de estiércol que sirva para fermentar."
+        }, {
+          "texto":"La segunda capa tendrá restos orgánicos. Es importante alternar las capas de materiales secos con materiales húmedos. Añadir capas de compost viejo o de estiércol que actúe como levadura es recomendable."
+        }, {
+          "texto":"Después habrá que removerlo y dejarlo reposar. Airearlo de vez en cuando y regarlo cuando esté seco será otro detalle a tener en cuenta."
+        }, {
+          "texto":"Varios meses después (ente 4 y 6), las capas inferiores estarán compostadas. Debería tener un color oscuro, textura suelta y un olor agradable."
+        }
+      ],
+
+      "curiosidad_Marron":[
+        {
+          "texto":"El compost es el abono formado por la mezcla de residuos orgánicos fermentados y materias minerales. El compost per se existe desde el nacimiento de las plantas verdes en la tierra."
+        }, {
+          "texto":"Cualquier vegetal que acabe en el suelo y se pudra con participación de oxígeno se transforma en composto. Sin embargo, el que no tiene la ayuda del oxígeno, se convierte en turba (carbón ligero de aspecto terroso y esponjoso) y finalmente en carbón por el efecto de la presión."
+        }, {
+          "texto":"Se dice que el primer compost realizado por el ser humano y no la naturaleza fue sir Albert Howard, que preparaba su compost con una capa de material vegetal de 15 centímetros."
+        }, {
+          "texto":" Desde entonces se han ideado muchas recetas y maneras de hacerlo. Siendo una materia prima tan potente ¿por qué no aprovecharla?"
+        }, {
+          "texto":"Más del 40% de nuestras bolsas de basura incluyen bioresiduos que son cien por cien aprovechables. Es posible que aún no tengas un contenedor marrón cerca, en ese caso, puedes aprender a hacer tu propio compost en casa o colegio para el huerto o jardín."
+        }, {
+          "texto":"Es importante meter bolsas en los contenedores, para mantener con mayor facilidad la higiene del lugar. Si el espacio no te permite añadir un cubo más, siempre puedes colocar bolsas independientes en el mismo. Además, para poder facilitar la diferenciación hasta que todos y todas nos acostumbremos, pueden colocarse etiquetas con nombres, colores e incluso dibujos."
+        }
+      ],
+
+      "depositar_PuntoLimpio":[
+        {
+          "texto":"En esencia, se trata de tirar aquí todo aquello que no pueda tirarse en contenedores convencionales. Los puntos limpios sólo admiten residuos generados por particulares y no de origen industrial."
+        }, {
+          "texto":"Algunos de los materiales que pueden llevarse son pilas, todo tipo de baterías, electrodomésticos, restos de pintura, aceites (tanto de cocina como de motor), halógenos o lamparas fluorescentes, muebles y objetos voluminosos, aerosoles y productos químicos, medicamentos, escombros, chatarra, colchones, termómetros o radiografías entre otras."
+        }, {
+          "texto":"A la entrada de los puntos limpios, un operario nos dará toda la información necesaria para realizar el deposito de cada uno de los residuos correctamente."
+        }
+      ],
+
+      "error_PuntoLimpio":[
+        {
+          "texto":"Ha de tenerse en cuenta que no todos los puntos limpios aceptan los mismos objetos, así que lo mejor es informarse. Puedes buscar la información en el ayuntamiento o acercarte a tu punto limpio más cercano para comprobarlo."
+        }, {
+          "texto":"Algunos de los materiales que no se admiten en los puntos limpios son: residuos orgánicos, materiales radioactivos, residuos infecciosos, neumáticos, materiales explosivos, residuos sin identificar o sin segregar. ¡A tener en cuenta!"
+        }
+      ],
+
+      "reciclar_PuntoLimpio":[
+        {
+          "texto":"Los puntos limpios han pasado a ser un importante elemento en las cadenas de reciclado de distintas corrientes de residuos, y recientemente considerados como instrumentos para realizar acciones de prevención y puntos de formación y concienciación ambiental."
+        }, {
+          "texto":"La mayor parte de los residuos que se recogen se trasladan a instalaciones de reciclaje ya existentes. Los demás, aquellos que no se pueden reciclare, son tratados o eliminados de la manera más adecuada posible en las instalaciones de la Consejería de Medio Ambiente y Ordenación del Territorio."
+        }
+      ],
+
+      "curiosidad_PuntoLimpio":[
+        {
+          "texto":"En la actualidad existen en España puntos limpios tanto fijos como móviles, aunque no se distribuyen de manera uniforme."
+        }, {
+          "texto":"Existen 1.739 puntos limpios fijos en España, y 189 móviles, siendo y Cataluña en primer lugar, y en segundo Castilla la Mancha, las comunidades autónomas que más poseen. El ratio medio en toda España de Puntos Limpios totales es de 24.445 habitantes por punto limpio."
+        }, {
+          "texto":"El flujo más significativo en peso de desechos en los puntos limpios pertenece a los escombros, y en las ciudades, la aportación es menor por habitante en comparación que en otros lugares."
+        }, {
+          "texto":"Los puntos limpios son esos impresionantes lugares donde podemos encontrar una infinidad de tipos de residuos. Se recogen y almacenan temporalmente de forma separada porque podrían ser grandes o peligrosos si se arrojasen en los contenedores convencionales. Los principales tipos son:"
+        }, {
+          "texto":"Los puntos limpios fijos son aquellos que, aun teniendo dimensiones diferentes, se encuentran en zonas no centrales de los municipios."
+        }, {
+          "texto":"Los mini puntos limpios o puntos limpios de barrio se ubican en zonas más cercanas al usuario, para que puedan hacer mayor uso de ellos. Al ser más pequeños, no pueden aportar los mismos servicios que los primeros."
+        }, {
+          "texto":"Los puntos limpios móviles son servicios dados por un vehículo de recogida con compartimentos para los distintos residuos, para ofrecer, normalmente, servicio a zonas pequeñas."
+        }
+      ],
+
+      "depositar_Sigre":[
+        {
+          "texto":""
+        }, {
+          "texto":""
+        }
+      ],
+
+      "error_Sigre":[
+        {
+          "texto":""
+        }
+      ],
+
+      "reciclar_Sigre":[
+        {
+          "texto":""
+        }, {
+          "texto":""
+        }, {
+          "texto":""
+        }, {
+          "texto":""
+        }, {
+          "texto":""
+        }, {
+          "texto":""
+        }, {
+          "texto":""
+        }, {
+          "texto":""
+        }
+      ],
+
+      "curiosidad_Sigre":[
+        {
+          "texto":""
+        }, {
+          "texto":""
+        }, {
+          "texto":""
+        }, {
+          "texto":""
+        }, {
+          "texto":""
+        }, {
+          "texto":""
+        }
       ],
     }
 };
