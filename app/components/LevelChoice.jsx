4d22a63cdb853277047a694269c22843d040fece
Diego Stewart <diegostewart1995@gmail.com>
2019-05-25 19:33:29 +0200

    Añadiendo Modal para Info

--- app/components/LevelChoice.jsx
+++ app/components/LevelChoice.jsx
@@ -2,9 +2,28 @@ import React from 'react';
 import './../assets/scss/level_choice.scss';
 import {renderScreen} from '../reducers/actions.jsx'
 
-import {Row,Col, Container} from 'react-bootstrap';
+import {Row,Col,Container,Modal,Button} from 'react-bootstrap';
 
 export default class LevelChoice extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+
+    this.state = {
+      show: false,
+    }
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
 
   render(){
     
@@ -14,7 +33,9 @@ export default class LevelChoice extends React.Component {
       <div className="levelChoiceWrapper">
         <div className="infoButtonWrapper">
           <a className="info" onClick={this.props.onSelectScreen.bind(this,2,0)}><img src={"assets/images/Interfaz/info.png"} height="50" /></a>
-          <a className="git" target="_blank" href="https://github.com/diegostewart/RESCORM"><img src="assets/images/Interfaz/IconoQuiz.png" height="50" /></a>
+          <a className="information" onClick={this.handleShow}><img src={"assets/images/Interfaz/information.png"} height="15" /></a>
+
+          {/* <a className="git" target="_blank" href="https://github.com/diegostewart/RESCORM"><img src="assets/images/Interfaz/IconoQuiz.png" height="50" /></a> */}
         </div>
         <h1>Bienvenido al Juego Interactivo de Reciclaje</h1>
         <div className="levelChoiceButton">
@@ -25,6 +46,25 @@ export default class LevelChoice extends React.Component {
             <Col> <button className="levelSelect3"  onClick={this.props.onSelectScreen.bind(this,1,3)} >NIVEL 3</button> </Col>
           </Row>
           </Container>
+
+
+          <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Información Adicional</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>El código es de dominio público y puede visitarlo en <a href="https://github.com/diegostewart/RESCORM"> GitHub. </a></p>
+            <p>La aplicación tambien será accesible mediante <a href="https://github.com/diegostewart/RESCORM"> VisHub. </a></p>
+            <p>Debo dar atribución a los recursos utilizados.</p>
+            <a href="https://www.vecteezy.com">Free Vector Graphics by www.Vecteezy.com</a>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
         </div>
       </div>
     );
